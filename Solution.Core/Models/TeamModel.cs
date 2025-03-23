30f7b84b1ab3ff9b7dbf1094d64ab9f69bc866a8
Polyák Zalán <polyakzalan394@gmail.com>
2025-03-23 12:14:06 +0100

    Member validation / Models done

--- Solution.Core/Models/TeamModel.cs
+++ Solution.Core/Models/TeamModel.cs
@@ -8,10 +8,14 @@
 
         public ValidatableObject<CompetitionModel> Competition { get; set; }
 
+        public ValidatableObject<List<MemberModel>> Members { get; set; }
+
         public TeamModel()
         {
             this.Name = new ValidatableObject<string>();
             this.Competition = new ValidatableObject<CompetitionModel>();
+            this.Members = new ValidatableObject<List<MemberModel>>();
+            this.Members.Value = new List<MemberModel>();
 
             AddValidators();
         }
@@ -21,6 +25,7 @@
             this.Id = entity.PublicId;
             this.Name.Value = entity.Name;
             this.Competition.Value = new CompetitionModel(entity.Competition);
+            this.Members.Value = entity.Members.Select(m => new MemberModel(m)).ToList();
         }
 
         public TeamEntity ToEntity()
@@ -29,7 +34,8 @@
             {
                 PublicId = Id,
                 Name = Name.Value,
-                CompetitionId = uint.Parse(Competition.Value.Id)
+                CompetitionId = uint.Parse(Competition.Value.Id),
+                Members = Members.Value.Select(m => m.ToEntity()).ToList()
             };
         }
 
@@ -38,14 +44,19 @@
             entity.PublicId = Id;
             entity.Name = Name.Value;
             entity.CompetitionId = uint.Parse(Competition.Value.Id);
+            entity.Members = Members.Value.Select(m => m.ToEntity()).ToList();
         }
-
 
         private void AddValidators()
         {
             this.Name.Validations.Add(new IsNotNullOrEmptyRule<string>
             {
                 ValidationMessage = "Name is required"
+            });
+
+            this.Members.Validations.Add(new MaxMembersRule<MemberModel>
+            {
+                ValidationMessage = "A team can have a maximum of 10 members"
             });
         }
     }
---
+++ Solution.ValidationLibrary/ValidationRules/MaxMemberRule.cs
@@ -0,0 +1,13 @@
+﻿namespace Solution.ValidationLibrary.ValidationRules
+{
+    public class MaxMembersRule<T> : IValidationRule<List<T>>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(object value)
+        {
+            var members = value as List<T>;
+            return members != null && members.Count <= 10;
+        }
+    }
+}
